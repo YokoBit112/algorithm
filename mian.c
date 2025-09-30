bd2ca9799b2eec708b3df7429f917ed3c3a20461
aaofovo <hp340g7@12344.com>
2025-09-30 22:15:03 +0800

    尝试变基

--- mian.c
+++ mian.c
@@ -1,18 +1,8 @@
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
 
 int main(void) {
-	int t;
-	if(!(cin >> t))return 0;
-	while (t--) {
-		long long n, m, k,ans,tot;
-		cin >> n >> m >> k;
-		tot = n * m;
-		ans = tot - k + 1;
-		if (ans>=0)
-			cout << ans << endl;
-		else cout << 0 << endl;
-	}
+	char c;
+	scanf_s("%c", &c);
+	printf("%c", c);
 	return 0;
 }
